c0253865df480ab9b676616095bf1a1fb07d524d
Wira94 <58247640+Wira94@users.noreply.github.com>
2021-06-09 21:26:44 -0500

    Update Prueba_Desarrollador2.php

--- Prueba_Desarrollador2.php
+++ Prueba_Desarrollador2.php
@@ -3,4 +3,7 @@ echo "Creado desde la rama desarollador2";
 echo "Continuamos el siguiende domingo";
 echo "avanzamemos el proyecto";
 echo "Ya deben tener instalado el proyecto en su local";
+const USUARIO = "ADMIN";
+const PASSWORD = "159147159";
+$numero = 12.1;
 ?>
